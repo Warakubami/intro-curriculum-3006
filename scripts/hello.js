91eab9880a07c3d2364f2e83b6bf104708b4dcbc
Warakubami <na3na7ko1@gmail.com>
2020-09-25 15:54:59 +0900

    おみくじ

--- scripts/hello.js
+++ scripts/hello.js
@@ -7,7 +7,7 @@ module.exports = (robot) => {
 
 	robot.hear(/おみくじ/i, msg => {
 		const user_id = msg.message.user.id;
-		const lots = [];
+		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(`${lot}, <@${user_id}>`);
 	})
